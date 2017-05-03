de66ddfd4d521199185180e61d9971f1a529020d
Marcos Mori <gigio0107@gmail.com>
2017-05-03 11:29:38 -0300

    limpando codigo

--- bireme/valida-mobile-iahx.py
+++ bireme/valida-mobile-iahx.py
@@ -4,27 +4,17 @@
 # checar nesses arquivos o marcador <mobile_version>true</mobile_version>
 # Listar os arquivos config.xml que nao tem esse marcador true
 
-
-
-# doc = http://stackoverflow.com/questions/3893885/cheap-way-to-search-a-large-text-file-for-a-string
-# doc = http://www.carlissongaldino.com.br/post/como-baixar-um-arquivo-com-python
-# no site acima te da varias possibilidade de baixar - vou utilizar a urllib2
 import urllib2
-
 
 instancias = ['portal', 'homeopatia', 'brasil', 'saudepublica']
 
 i = -1
 while i < 3:
 
-        #  url = 'http://pesquisa.bvsalud.org/%s/config/config.xml' % i
-        # url = urllib2.Request('http://pesquisa.bvsalud.org/%s/config/config.xml' % i)
         url = ('http://pesquisa.bvsalud.org/%s/config/config.xml' % instancias[i])
         # print url
-        # print "baixando com urllib2"
         f = urllib2.urlopen(url)
         data = f.read()
-        # with open("config.xml",  "wb") as code:
         nome = ('%s-config.xml' % instancias[i])
 	print nome
 	with open(nome,  "wb") as code:
