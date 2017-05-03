4fbb2e4af8ce89da0b31af85b0a5f73d684f9406
Marcos Mori <gigio0107@gmail.com>
2017-05-03 17:41:57 -0300

    lendo xml

--- bireme/valida-mobile-iahx.py
+++ bireme/valida-mobile-iahx.py
@@ -5,6 +5,8 @@
 # Listar os arquivos config.xml que nao tem esse marcador true
 
 import urllib2
+import xml.etree.ElementTree as ET
+
 
 instancias = ['portal', 'homeopatia', 'brasil', 'saudepublica']
 
@@ -20,4 +22,25 @@ for (i,item) in enumerate(instancias):
 	# print nome
 	with open(nome,  "wb") as code:
 		code.write(data)
-        i = i + 1
+	#print nome
+        #e = xml.etree.ElementTree.parse(nome).getroot()
+	#print e
+	tree = ET.parse(nome)
+	root = tree.getroot()
+	#print tree
+	#print root
+	#for neighbor in root.iter('mobile_version'):
+	#	print neighbor.attrib
+	#	print '---------------------------------'
+	#for child in root:
+	#	#print child.tag, child.attrib
+	#	print child.tag	
+	# print '---------------------------------'
+	for mobile_version in root.findall('mobile_version'):
+		rank = mobile_version.find('true').txt
+		name = mobile_version.get('true')
+	print name, rank
+
+	i = i + 1
+#https://docs.python.org/2/library/xml.etree.elementtree.html
+	
